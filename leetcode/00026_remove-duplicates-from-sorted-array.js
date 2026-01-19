5b409ff225a82196c3b24e43bbffcebcc32c4cc5
miyinderick <miyinderick@qq.com>
2026-01-19 15:47:10 +0800

    26题，使用快慢指针解决

---
+++ leetcode/00026_remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  const changdu=nums.length;
+  let j=0;
+  for(let i=1;i<nums.length;i++){
+      if(nums[i]!=nums[j]){
+        j++;
+        nums[j]=nums[i];
+      }
+  }
+
+    return j+1;
+};
